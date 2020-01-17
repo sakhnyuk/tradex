dd86d3a283b364b41068bd39373000ae70c77232
Mikhail Sakhnyuk <sakhnyukmikhail@gmail.com>
2020-01-18 02:24:59 +0700

    Create appConstant.ts

---
+++ src/renderer/appConstant.ts
@@ -0,0 +1,35 @@
+export const exchanges = [
+  'binance',
+  'bitfinex',
+  'bitmex',
+  'bitmex-testnet',
+  'cryptobridge',
+  'hitbtc',
+  'okex',
+  'poloniex',
+  'crex24',
+  'huobi-global',
+  'exmo',
+];
+
+export const privateExchanges = ['binance', 'bitfinex', 'cryptobridge', 'crex24', 'okex', 'hitbtc', 'huobi-global'];
+
+export const marginExchanges = ['bitmex', 'bitmex-testnet'];
+
+export const arbitrageExchanges = ['binance', 'bitfinex', 'exmo'];
+
+export const exchangePairs = {
+  binance: 'BTC/USDT',
+  bitfinex: 'BTC/USD',
+  bitmex: 'XBT/USD',
+  'bitmex-testnet': 'XBT/USD',
+  cryptobridge: 'ETH/BTC',
+  hitbtc: 'BTC/USD',
+  okex: 'BTC/USDT',
+  poloniex: 'BTC/USDT',
+  crex24: 'BTC/USD',
+  'huobi-global': 'BTC/USDT',
+  exmo: 'BTC/USD',
+};
+
+export const storeVersion = 2006;
