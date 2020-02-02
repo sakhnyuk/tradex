a8ee47d97d5150b5cfa1dc1c86f0d7225e98d5bb
Mikhail Sakhnyuk <sakhnyukmikhail@gmail.com>
2020-02-02 17:03:08 +0700

    Update core selector

--- src/renderer/store/core/selectors.ts
+++ src/renderer/store/core/selectors.ts
@@ -1,19 +1,21 @@
 import { Store } from '../types';
 
-export const selectIsOnline = (state: Store) => state.core.isOnline;
-export const selectReload = (state: Store) => state.core.reloadChart;
-export const selectTheme = (state: Store) => state.core.theme;
-export const selectIcons = (state: Store) => state.core.icons;
-export const selectIsMac = (state: Store) => state.core.isMac;
+export const selectCore = {
+  isOnline: (state: Store) => state.core.isOnline,
+  reloadChart: (state: Store) => state.core.reloadChart,
+  theme: (state: Store) => state.core.theme,
+  icons: (state: Store) => state.core.icons,
+  isMac: (state: Store) => state.core.isMac,
 
-// ui state
-export const selectSettingsPage = (state: Store) => state.core.settingPage;
-export const selectIsSettingsOpen = (state: Store) => state.core.settingOpen;
-export const selectIsPairBarOpen = (state: Store) => state.core.pairsBarOpen;
-export const selectHistoryComponent = (state: Store) => state.core.historyComponent;
-export const selectShowAllAccounts = (state: Store) => state.core.showAllAccounts;
-export const selectShowAllPositions = (state: Store) => state.core.showAllPositions;
-export const selectShowAllPairs = (state: Store) => state.core.showAllPairs;
-export const selectOrderBookIsLoading = (state: Store) => state.core.orderBookIsLoading;
-export const selectTradesIsLoading = (state: Store) => state.core.tradesIsLoading;
-export const selectPairListSorting = (state: Store) => state.core.pairListSorting;
+  // ui state
+  settingPage: (state: Store) => state.core.settingPage,
+  settingOpen: (state: Store) => state.core.settingOpen,
+  pairsBarOpen: (state: Store) => state.core.pairsBarOpen,
+  historyComponent: (state: Store) => state.core.historyComponent,
+  showAllAccounts: (state: Store) => state.core.showAllAccounts,
+  showAllPositions: (state: Store) => state.core.showAllPositions,
+  showAllPairs: (state: Store) => state.core.showAllPairs,
+  orderBookIsLoading: (state: Store) => state.core.orderBookIsLoading,
+  tradesIsLoading: (state: Store) => state.core.tradesIsLoading,
+  pairListSorting: (state: Store) => state.core.pairListSorting,
+};
