4338bedbdaafb87b6fbf13b8229e2b628c48ec76
Mikhail Sakhnyuk <sakhnyukmikhail@gmail.com>
2020-02-03 16:17:47 +0700

    Add online status listener

--- src/renderer/App/index.tsx
+++ src/renderer/App/index.tsx
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MuiThemeProvider, makeStyles } from '@material-ui/core';
 import { ConnectedRouter } from 'connected-react-router';
 import { Switch, Route } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
+import { ipcRenderer } from 'electron';
 import { createTheme } from '../theme/createTheme';
 import { history } from '../store/configureStore';
-import styles from './styles';
+
 import MacHeader from '../components/Header/MacOS';
 import WinHeader from '../components/Header/Windows';
 import { isMac } from '../appConstant';
 import LeftMenu from '../components/LeftMenu';
 import Analysis from '../components/Analysis';
+import { selectCore } from '../store/core/selectors';
+import { useCoreActions } from '../store/core/useCoreActions';
+
+import styles from './styles';
 
 const useStyles = makeStyles(styles);
 
 export const App: React.FC = () => {
-  const theme = 'light';
   const classes = useStyles();
+
+  const { setOnline } = useCoreActions();
+  const theme = useSelector(selectCore.theme);
+
+  useEffect(() => {
+    // Internet connection checker. Dispatching online status
+    // use when connection is OFF, we close all sockets and intervals.
+    // When ON - turn on socket and rest
+    const update = () => {
+      ipcRenderer.send('status-changed', navigator.onLine);
+      setOnline(navigator.onLine);
+    };
+    window.addEventListener('online', update);
+    window.addEventListener('offline', update);
+    update();
+
+    return () => {
+      window.removeEventListener('online', update);
+      window.removeEventListener('offline', update);
+    };
+  }, [setOnline]);
 
   return (
     <MuiThemeProvider theme={createTheme(theme)}>
